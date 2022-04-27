100041a7192058290482df9fdab6b164ac74e0da
raheemadamboev <vividricemaryandmax@gmail.com>
2022-04-27 13:55:41 +0500

    setup ImageListFragment.kt

---
+++ app/src/main/java/xyz/teamgravity/imagetextadder/presentation/fragment/ImageListFragment.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.imagetextadder.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.teamgravity.imagetextadder.databinding.FragmentImageListBinding
+
+class ImageListFragment : Fragment() {
+
+    private var _binding: FragmentImageListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentImageListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

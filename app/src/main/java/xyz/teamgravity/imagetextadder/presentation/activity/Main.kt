88674a752e9b8965b3f667318249a5ea00d097ea
raheemadamboev <vividricemaryandmax@gmail.com>
2022-04-27 14:06:49 +0500

    setup Main.kt

--- app/src/main/java/xyz/teamgravity/imagetextadder/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/imagetextadder/presentation/activity/Main.kt
@@ -2,6 +2,8 @@ package xyz.teamgravity.imagetextadder.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import xyz.teamgravity.imagetextadder.R
 import xyz.teamgravity.imagetextadder.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
@@ -9,8 +11,13 @@ class Main : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragment_container).navigateUp()
     }
 }
